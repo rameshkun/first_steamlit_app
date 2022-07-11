4862acd81eaf5b57aa83341edf225102c72e5c75
Ramesh kunwar <109100672+rameshkun@users.noreply.github.com>
2022-07-11 18:57:45 +0100

    Update streamlit_app.py
    Added new more contents

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
-streamlit.title('My parents new healthy reason!!')
-
-streamlit.title('New born baby!!')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
