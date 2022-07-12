18e86e974b08dbc1bd6dcc5251b91f1fdb4e8e47
Ramesh kunwar <109100672+rameshkun@users.noreply.github.com>
2022-07-12 14:17:57 +0100

    Update streamlit_app.py
    Added the option field to select fruit to me shown

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,11 @@ streamlit.dataframe(fruits_to_show)
 #New section to display Fruityvice API Response
 streamlit.header('Fruityvice Fruit Advice!')
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 # convert json file to normalize form
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
